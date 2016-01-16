8c7df0007fb42444be637f7066b41d4fe3f21e70
daniel-ballard <dballard23@ufl.edu>
2016-01-15 22:33:18 -0500

    Added functionality

--- listingController.js
+++ listingController.js
@@ -1,14 +1,31 @@
 angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
   function($scope, Listings) {
     $scope.listings = Listings;
-    $scope.detailedInfo = undefined;
-
+    $scope.selected = "";
+    $scope.search_filter = "";
+    $scope.newItem = {};
+    $scope.coordinates = {};
     /* 
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+
+    $scope.addListing = function() {
+        
+        // Submit
+        $scope.listings.push($scope.newItem);
+    };
+
+    $scope.deleteListing = function(index) {
+        // Deselect if selected and use splice to remove from array
+        if ($scope.selected = $scope.listings[index])
+            $scope.selected = "";
+        $scope.listings.splice(index,1);
+    };
+
+    $scope.showDetails = function(index) {
+        // Put selected listing item in $scope.selected
+        $scope.selected = $scope.listings[index];
+    };
   }
 ]);
