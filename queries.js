11a49b5537e767847430d8d9feffb51c3386da4e
daniel-ballard <dballard23@ufl.edu>
2016-01-20 17:17:12 -0500

    Added second and third queries

--- queries.js
+++ queries.js
@@ -20,13 +20,33 @@ var removeCable = function() {
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
     and remove this listing from your database and log the document to the console. 
    */
+  Listing.remove({'code':'CABL'}, function(err, result) {
+    if (err) { console.log(err); }
+    console.log(result);
+  });
 };
+
 var updatePhelpsMemorial = function() {
   /*
     Phelps Memorial Hospital Center's address is incorrect. Find the listing, update it, and then 
     log the updated document to the console. 
    */
+  var new_record = {
+    'code': 'PHL',
+    'name': 'Phelps Laboratory',
+    'coordinates': {
+      'latitude': 41.1091195,
+      'longitude': -73.8639555,
+    },
+    'address': 'Gainesville, FL 32611'
+  }
+
+  Listing.update({'code':'PHL'}, new_record, function(err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
 };
+
 var retrieveAllListings = function() {
   Listing.find(function(err,listings) {
 	  if(err) return console.error(err);
